c2b8ad2547e8ac550185802774ba0a64c925a5c8
Stolar <stolar28@gmail.com>
2019-05-10 15:25:11 +0200

    CarRepository, DPTO Context

---
+++ DPTO.Infrastructure/DptoContext.cs
@@ -0,0 +1,14 @@
+﻿using DPTO.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DPTO.Infrastructure
+{
+    public class DptoContext : DbContext
+    {
+        public DptoContext(DbContextOptions<DptoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
